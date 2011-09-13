3d2bf8b5f439cbb19784114b0ded9d95a7333f5b
James Cunningham <j.a.cunningham@gmail.com>
2011-09-13 12:15:20 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject genetic "0.1"
+  :description "A genetic programming framework"
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ test/genetic/test/core.clj
@@ -0,0 +1,6 @@
+(ns genetic.test.core
+  (:use [genetic.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
